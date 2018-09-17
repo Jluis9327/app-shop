6c214b04fa1a2e0f38b1132865f0ef608014827d
elgatofel <j.luis9327@gmail.com>
2018-09-17 00:42:52 -0500

    Segundo commit

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -1,14 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
-
-
+use App\Product;
 
 class TestController extends Controller
 {
     //
     public function welcome()
     {
-        return view('welcome');
+        $products=Product::all();
+        return view('welcome')->with(compact('products'));
     }
 }
---
+++ database/seeds/ProductTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ProductTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        /*factory(App\Category::class,5)->create();
+        factory(App\Product::class,100)->create();//crea y lo guarda en la base de datos
+        factory(App\ProductImage::class,200)->create();*/
+        
+
+    }
+}
